cff13ef7ab2914ca904c376844d82ab50fbe9028
kiku ishimoto <kikuish@gmail.com>
2020-04-07 05:14:15 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,15 +1,13 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 
-function addElementToBeginningOfArray() {
-  var myArray = ['1', '2', '3'];
-  return ['0', ...myArray];
+function addElementToBeginningOfArray(array, element) {
+  returm [element, ...array]
 
 }
 
-function destructivelyAddElementToBeginningOfArray() {
-  var myArray = ['1', '2', '3']
-  myArray.unshift('0')
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
 
-  myArray
+  return array
 }
